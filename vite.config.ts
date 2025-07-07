016d8d982be911322516d8f43e23e53f4fed93cd
ruishalm <rafa.cmunhoz@hotmail.com>
2025-07-06 22:44:26 -0300

    brachs multiplas para clientes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// O nome do seu repositório no GitHub
+const repoName = 'sitePessoalModeloi';
+
+// Lógica para definir o caminho base dinamicamente
+const getBase = () => {
+  const githubRefName = process.env.GITHUB_REF_NAME;
+  // Para builds no GitHub Actions
+  if (githubRefName) {
+    return githubRefName === 'main'
+      ? `/${repoName}/`
+      : `/${repoName}/${githubRefName}/`;
+  }
+  // Para desenvolvimento local
+  return '/';
+};
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: getBase(),
   plugins: [react()],
 })
